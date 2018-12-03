3007f01fceca3372897125489e161dcabe507443
Ajay Padvi <ajaypadvi@gmail.com>
2018-12-03 22:47:35 +0530

    Refactored the project,Create two separate modules kafka-basics and kafka-producer-twitter

--- kafka-basics/src/main/java/com/ajay/kafka/tutorial1/ConsumerDemo.java
+++ kafka-basics/src/main/java/com/ajay/kafka/tutorial1/ConsumerDemo.java
@@ -1,9 +1,10 @@
+package com.ajay.kafka.tutorial1;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,33 +14,36 @@ import java.util.Properties;
 
 public class ConsumerDemo {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
-
     public static void main(String[] args) {
-        final String bootstrapServers = "127.0.0.1:9092";
-        final String groupId = "my-fourth-application";
-        final String topic = "first_topic";
 
-        //Create a consumer config/properties
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "first_topic";
+
+        // create consumer configs
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //other options are latest and none
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        //Create Consumer
+        // create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
-        //Subscribe consumer to our topics
+        // subscribe consumer to our topic(s)
         consumer.subscribe(Arrays.asList(topic));
 
-        //Poll for new data
-        while (true) {
-            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : consumerRecords) {
-                logger.info("Key: " + record.key() + " ,Value: " + record.value());
-                logger.info("Partition: " + record.partition() + " ,Offset: " + record.offset());
+        // poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record : records){
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
             }
         }
 
--- kafka-basics/src/main/java/com/ajay/kafka/tutorial1/ProducerDemo.java
+++ kafka-basics/src/main/java/com/ajay/kafka/tutorial1/ProducerDemo.java
@@ -1,3 +1,5 @@
+package com.ajay.kafka.tutorial1;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -6,28 +8,31 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 public class ProducerDemo {
+
     public static void main(String[] args) {
-        final String bootstrapServers = "127.0.0.1:9092";
-        final String topicName = "first_topic";
 
-        //Create the producer config
+        String bootstrapServers = "127.0.0.1:9092";
 
+        // create Producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        //Create the producer
+        // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        //Create producer record
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName, "hello world!!");
+        // create a producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello world");
 
-        //Send data
+        // send data - asynchronous
         producer.send(record);
 
-        //flush and close the producer
+        // flush data
         producer.flush();
+        // flush and close producer
         producer.close();
+
     }
 }
